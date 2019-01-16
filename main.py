368b3c1cc0b31e5d6db40165213979a52569d4f8
Joonyoung Kim <kimjymcl@snu.ac.kr>
2019-01-16 14:46:56 +0900

    Learning a sequence of tasks

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+from absl import flags
 import numpy as np
 import tensorflow as tf
 import dataset as mnist
 
 
-def train_input_fn(batch_size):
-    dataset_train, _ = mnist.load_mnist_datasets()
-    dataset_train = dataset_train.shuffle(1000).repeat(5).batch(batch_size)
+# Task flags
+flags.DEFINE_integer('n_task', '10', 'Number of tasks in sequence.')
+flags.DEFINE_integer('seed', '1', 'Random seed.')
 
-    return dataset_train
+# Estimator flags
+flags.DEFINE_string('model_dir', 'model', 'Path to output model directory.')
+flags.DEFINE_integer('n_epoch', 1, 'Number of train epochs.')
+
+# Optimizer flags
+flags.DEFINE_float('lr', 1e-1, 'Learning rate of an optimizer.')
+flags.DEFINE_integer('n_batch', 10, 'Number of examples in a batch')
+
+FLAGS = flags.FLAGS
 
 
-def eval_input_fn(batch_size):
-    _, dataset_eval = mnist.load_mnist_datasets()
-    dataset_eval = dataset_eval.batch(batch_size)
+def prepare_permutations(n_task, seed):
+    np.random.seed(seed)
+    n_pixel = 784
+    p = np.zeros((n_task, n_pixel))
+    for i in range(n_task):
+        p[i] = np.random.permutation(n_pixel)
 
-    return dataset_eval
+    p = p.astype(np.int32)
+
+    return p
+
+
+def train_input_fn(n_epoch, n_batch, p):
+    d_train, _ = mnist.load_mnist_datasets()
+    perm_d_train = d_train.map(lambda feature, label: (mnist.permute(feature, p), label))
+    perm_d_train = perm_d_train.repeat(n_epoch).batch(n_batch)
+
+    return perm_d_train
+
+
+def eval_input_fn(n_batch, p):
+    _, d_eval = mnist.load_mnist_datasets()
+    perm_d_eval = d_eval.map(lambda feature, label: (mnist.permute(feature, p), label))
+    perm_d_eval = perm_d_eval.batch(n_batch)
+
+    return perm_d_eval
 
 
 def model_fn(features, labels, mode, params):
     model = FullyConnectedNetwork()
-    logits = model(features)
+    logits= model(features)
     predictions = tf.argmax(logits, axis=1)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -34,7 +64,7 @@ def model_fn(features, labels, mode, params):
         accuracy = tf.metrics.accuracy(labels, predictions)
         return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops={'accuracy': accuracy})
 
-    opt = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    opt = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
 
     train_op = opt.minimize(loss, global_step=tf.train.get_global_step())
 
@@ -46,22 +76,32 @@ class FullyConnectedNetwork(tf.keras.models.Model):
         super(FullyConnectedNetwork, self).__init__()
         self.net = tf.keras.Sequential([
             tf.keras.layers.InputLayer((784,)),
-            tf.keras.layers.Dense(800, activation='relu'),
-            tf.keras.layers.Dense(800, activation='relu'),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(100, activation='relu'),
             tf.keras.layers.Dense(10)])
 
     def call(self, inputs, training=None, mask=None):
         return self.net(inputs)
 
 
-if __name__ == "__main__":
+def main(unused_argv):
+    params = {'learning_rate': FLAGS.lr}
+    n_task = FLAGS.n_task
+    run_config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, save_checkpoints_steps=2000)
 
-    batch_size = 64
-    run_config = tf.estimator.RunConfig(model_dir='model', save_checkpoints_steps=500)
+    estimator = tf.estimator.Estimator(model_fn=model_fn, config=run_config, params=params)
+    p = prepare_permutations(n_task, FLAGS.seed)
 
-    estimator = tf.estimator.Estimator(model_fn=model_fn, config=run_config)
-    train_spec = tf.estimator.TrainSpec(input_fn=lambda: train_input_fn(batch_size=batch_size))
-    eval_spec = tf.estimator.EvalSpec(input_fn=lambda: eval_input_fn(batch_size=batch_size),
-                                      start_delay_secs=1, throttle_secs=1)
+    accuracy_mat = np.zeros((n_task, n_task))
+    for i in range(n_task):
+        estimator.train(input_fn=lambda: train_input_fn(FLAGS.n_epoch, FLAGS.n_batch, p[i]))
+        for j in range(i+1):
+            result_dict = estimator.evaluate(input_fn=lambda: eval_input_fn(FLAGS.n_batch, p[j]))
+            accuracy_mat[i, j] = result_dict['accuracy']
 
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    print(accuracy_mat)
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
